46a33d048fa382537cd48d3e062f0c15875a7dc7
MosabAboidrees <mosab_mahala@yahoo.com>
2024-11-09 21:39:47 +0300

    Task 1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,7 @@ import logging
 from os import environ
 import mysql.connector
 
+
 def filter_datum(
                     fields: List[str],
                     redaction: str,
@@ -29,3 +30,33 @@ def filter_datum(
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class to filter specified fields."""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        """Initialize the formatter with fields to be redacted."""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        Format the log record by filtering specified fields.
+        Args:
+            record (logging.LogRecord): The log record to format.
+        Returns:
+            str: The formatted log message with specified fields redacted.
+        """
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
+        return super(RedactingFormatter, self).format(record)
+
+
+if __name__ == '__main__':
+    main()
