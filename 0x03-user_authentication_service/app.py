bc9f77d9b3f332a1e9ba9c6d4b56dc0b1d3d6940
MosabAboidrees <mosab_mahala@yahoo.com>
2024-11-22 00:24:26 +0300

    Task 11. Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -46,5 +46,32 @@ def users() -> str:
         return jsonify({"message": "email already registered"}), 400
 
 
+@app.route("/sessions", methods=["POST"])
+def login():
+    """
+    Handle POST request to log in a user.
+    Expects:
+        Form data with "email" and "password".
+    Returns:
+        JSON: A success message and sets the session ID as a cookie.
+    Raises:
+        401: If the login information is incorrect.
+    """
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    if not session_id:
+        abort(401)
+
+    response = make_response(jsonify({"email": email,
+                                      "message": "logged in"}), 200)
+    response.set_cookie("session_id", session_id)
+    return response
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
